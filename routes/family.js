db2ba5f2d96fc20b10c4028d66776c22d6f211ab
junyeongyu <Junyeong.Yu@MyGeorgian.ca>
2017-02-01 23:19:33 -0500

    Develop General Pages and error page. Adopt proper html styles. Adopt Bootstrap 3 style.

--- routes/family.js
+++ routes/family.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+	res.render('family/index');
+});
+router.get('/junyeong', function(req, res, next) {
+	res.render('family/junyeong');
+});
+router.get('/jungmyo', function(req, res, next) {
+	res.render('family/jungmyo');
+});
+router.get('/aeja', function(req, res, next) {
+	res.render('family/aeja');
+});
+router.get('/jongrak', function(req, res, next) {
+	res.render('family/jongrak');
 });
 
 module.exports = router;
